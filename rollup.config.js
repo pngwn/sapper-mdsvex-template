b97be7fbf9e4e02b90f5fe9a00a678a03ed8e8d6
Deklan Webster <deklanw@gmail.com>
2020-09-02 15:35:29 -0400

    Fix babel helper bug

--- rollup.config.js
+++ rollup.config.js
@@ -43,7 +43,7 @@ export default {
 			legacy &&
 				babel({
 					extensions: [".js", ".mjs", ".html", ...extensions],
-					runtimeHelpers: true,
+					babelHelpers: "runtime",
 					exclude: ["node_modules/@babel/**"],
 					presets: [
 						[
